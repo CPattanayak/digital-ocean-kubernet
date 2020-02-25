b520550a58932308e9053cb117d9edc0309e64d9
pattac <pattac@INGSPATTACL2C>
2020-02-26 01:01:14 +0530

    added logic to provission mongo

---
+++ provission_mongo/instance.tf
@@ -0,0 +1,47 @@
+resource "digitalocean_ssh_key" "default" {
+  name       = "mykey"
+  public_key = file("./mykey.pub")
+}
+
+resource "digitalocean_droplet" "replicaset" {
+  count              = 1
+  name               = "mongo${count.index}"
+  image              = "ubuntu-18-04-x64"
+  region             = "blr1"
+  size               = "s-2vcpu-2gb"
+  ssh_keys = [digitalocean_ssh_key.default.fingerprint]
+  private_networking = true
+  
+
+  provisioner "file" {
+	  connection {
+	    host = self.ipv4_address
+		type = "ssh"
+		user = "root"
+		agent = true
+		private_key = file("./mykey")
+	}
+   
+    destination = "/etc/mongod.conf"
+    content  = "${templatefile("${path.module}/templates/config.tmpl", {})}"
+  }
+
+  provisioner "remote-exec" {
+     connection {
+	    host = self.ipv4_address
+		type = "ssh"
+		user = "root"
+		agent = true
+		private_key = file("./mykey")
+	}
+    
+    inline = [
+      "wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -",
+      "echo 'deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list",
+      "apt-get update",
+      "apt-get install -y -o Dpkg::Options::=--force-confdef mongodb-org",
+	  "ufw allow 27017",
+      "service mongod start",
+    ]
+  }
+}
---
+++ provission_mongo/output.tf
@@ -0,0 +1,6 @@
+output "public_ips" {
+  value = digitalocean_droplet.replicaset.*.ipv4_address
+}
+#output "rs_config" {
+#  value = "${templatefile("${path.module}/templates/rs_conf.tmpl", { private_ips = digitalocean_droplet.replicaset.*.ipv4_address_private })}"
+#}
